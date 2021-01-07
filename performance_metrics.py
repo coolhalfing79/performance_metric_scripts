750dcc9459bc63934310fa33d2eb53782230af6b
coolhalfing79 <anirudhachari27@gmail.com>
2021-01-07 18:09:13 +0530

    "imporvements"

--- performance_metrics.py
+++ performance_metrics.py
@@ -1,9 +1,12 @@
 '''
 calculates rpl metrics from contiki cooja logfiles
 '''
+import csv
 import sys
 import os
 import re
+
+NODES = 0
 
 
 def main(argv):
@@ -12,68 +15,85 @@ def main(argv):
         logfile = argv[1]
         energylogfile = argv[2]
     except IndexError:
-        logfile = '/home/anirudha/final_year_project/mrhof100log.txt'
-        energylogfile = '/home/anirudha/final_year_project/test.txt'
-
+        print('no log files provided')
     if 'resultdir' not in os.listdir():
         os.mkdir('resultdir')
     resultdir = './resultdir'
 
-    boldline = '='* (len(logfile)+16)
-    thinline = '-'* (len(logfile)+16)
+    boldline = '=' * (len(logfile)+16)
+    thinline = '-' * (len(logfile)+16)
     print(boldline)
     print('RESULT DIRECTORY: ', resultdir)
     print('USING LOGFILE: ', logfile)
     print(boldline)
 
     metrics = (
-        (network_latency_reliability(logfile, resultdir), 'NETWORK LATENCY & PDR'),
-        (energyconsumption(energylogfile, resultdir), 'ENERGY CONSUMPTION'),
-        (networksetuptime(logfile, resultdir), 'NETWORK SETUP TIME'),
-        (networktraffic(logfile, resultdir), 'NETWORK TRAFFIC')
-
+        (networksetuptime(logfile), 'NETWORK SETUP TIME'),
+        (network_latency_reliability(logfile), 'NETWORK LATENCY & PDR'),
+        (energyconsumption(energylogfile), 'ENERGY CONSUMPTION'),
+        (networktraffic(logfile), 'NETWORK TRAFFIC')
     )
 
+    result = {'node count': NODES}
     for metric, name in metrics:
+        result.update(metric)
         print('\n'+boldline+'\n'+name+'\n' + thinline)
         for key, value in metric.items():
             print(key, '\t:', value)
 
+    if '70' in logfile:
+        n = '70'
+    elif '90' in logfile:
+        n = '90'
+    else:
+        n = '100'
+    resultfile = resultdir + f'/result{n}.csv'
+    with open(resultfile, 'a') as csvfile:
+        csvwriter = csv.DictWriter(csvfile, result.keys())
+        if not os.stat(resultfile).st_size:
+            csvwriter.writeheader()
+        csvwriter.writerow(result)
 
-def networksetuptime(logfile, resultdir):
+
+def networksetuptime(logfile):
     """ calculates network setup time as last DIO joined - first DIO sent"""
-    resultlog = resultdir + '/setup_time.txt'
+    node_count = 0
     metric = {
-        'node count': 0,
         'first DIO sent': 0,
-        'last DIO joined DAG': 0
+        'last DIO joined DAG': 0,
+        'network setup time': 0
     }
     with open(logfile, 'r') as readfile:
         for line in readfile:
             txt = re.findall(r'\d+', line)
-            time = int(txt[0]) * 60000 + int(txt[1]) * 1000 + int(txt[2])
-            if '-DIO with rank' in line:
-                if metric['first DIO sent'] == 0:
-                    metric['first DIO sent'] = time
-            elif 'RPL: Joined DAG with instance ID' in line:
-                metric['node count'] += 1
-                metric['last DIO joined DAG'] = time
+            if 'RPL' in line:
+                if len(txt) < 4:
+                    time = int(txt[0])
+                else:
+                    time = int(txt[0]) * 60000 + \
+                        int(txt[1]) * 1000 + int(txt[2])
+                if '-DIO with rank' in line:
+                    if metric['first DIO sent'] == 0:
+                        metric['first DIO sent'] = time
+                elif 'RPL: Joined DAG with instance ID' in line:
+                    node_count += 1
+                    metric['last DIO joined DAG'] = time
+    metric['network setup time'] = metric['last DIO joined DAG'] - \
+        metric['first DIO sent']
 
-    with open(resultlog, 'w') as resultfile:
-        resultfile.write(
-            f'{metric["first DIO sent"] - metric["last DIO joined DAG"]}\n')
+    global NODES
+    NODES += node_count
 
     return metric
 
 
-def networktraffic(logfile, resultdir):
+def networktraffic(logfile):
     """calculates number of DIO DIS and DAO sent"""
     metric = {
         'DIO sent': 0,
         'DIS sent': 0,
         'DAO sent': 0
     }
-    resultlog = resultdir + '/traffic.txt'
     with open(logfile) as file:
         for line in file:
             if 'DIO with rank' in line:
@@ -83,16 +103,11 @@ def networktraffic(logfile, resultdir):
             elif 'RPL: Sending DAO with prefix' in line:
                 metric['DAO sent'] += 1
 
-    with open(resultlog, 'w') as target:
-        target.write(
-            f'{metric["DIO sent"]}, {metric["DIS sent"]}, {metric["DAO sent"]}\n')
-
     return metric
 
 
-def network_latency_reliability(logfile, resultdir):
+def network_latency_reliability(logfile):
     """calculates PDR, average latency, send and recieved packets """
-    resultlog = resultdir + '/latency.txt'
     metric = {
         'PDR': 0,
         'lost packets': 0,
@@ -105,52 +120,46 @@ def network_latency_reliability(logfile, resultdir):
     with open(logfile) as file:
         for line in file:
             txt = re.findall(r"\d+", line)
-            time = int(txt[0])*60000 + int(txt[1])*1000 + int(txt[2])
-            if 'DATA send to' in line:
-                metric["sent packets"] += 1
-                try:
-                    message_data[txt[3]][txt[4]] = time
-                except KeyError:
-                    message_data[txt[3]] = {txt[4]: time}
-            elif 'DATA recv ' in line:
-                try:
-                    sendtime = message_data[txt[4]][txt[3]]
-                    metric["recieved packets"] += 1
-                    latency += time - sendtime
-                except KeyError:
-                    print(line)
+            if 'DATA' in line:
+                time = int(txt[0])*60000 + int(txt[1])*1000 + int(txt[2])
+                if 'send to' in line:
+                    metric["sent packets"] += 1
+                    try:
+                        message_data[txt[-2]][txt[-1]] = time
+                    except KeyError:
+                        message_data[txt[-2]] = {txt[-1]: time}
+                    #print('sent', txt[-3], '->', txt[-2])
+                elif 'recv ' in line:
+                    try:
+                        sendtime = message_data[txt[-1]][txt[-2]]
+                        metric["recieved packets"] += 1
+                        latency += time - sendtime
+                    except KeyError:
+                        pass
+                #print('recv', txt[-2], '<-', txt[-3])
 
     metric['lost packets'] = metric["sent packets"] - \
         metric["recieved packets"]
     try:
         metric['average latency'] = latency/metric["recieved packets"]
         metric['PDR'] = (metric["sent packets"] -
-                     metric['lost packets'])/metric["sent packets"] * 100
+                         metric['lost packets'])/metric["sent packets"] * 100
     except ZeroDivisionError:
-        sys.exit('wrong log file') # wrong log file may corrupt data 
-
-
-    with open(resultlog, 'w') as file:
-        file.write(f'{0},{1},{2},{3}'.format(
-            metric["sent packets"],
-            metric["average latency"],
-            metric["PDR"],
-            metric["lost packets"]))
+        sys.exit('wrong log file')  # wrong log file may corrupt data
 
     return metric
 
 
-def energyconsumption(logfile, resultdir):
+def energyconsumption(logfile):
     """
     energy comsumption is obtained by using powertrace
     powertrace and collect view cannot work simultaneousy
     without causing overflow so this is done separately and
     saved in a separate log file manually
     """
-    resultlog = resultdir + '/energy_consumption.txt'
     metric = {'total transmit ticks': 0,
               'total listen ticks': 0,
-              'total comsumption': 0,
+              'total consumption': 0,
               'total cpu': 0,
               'total lpm': 0,
               'radio on time': 0}
@@ -164,15 +173,13 @@ def energyconsumption(logfile, resultdir):
                 metric['total listen ticks'] += int(digits[11])
 
     try:
-        metric['radio on time'] += (metric['total transmit ticks'] + metric['total listen ticks'])/(
-            metric['total cpu'] + metric['total lpm']) * 100
+        metric['radio on time'] += ((metric['total transmit ticks'] + metric['total listen ticks'])/(
+            metric['total cpu'] + metric['total lpm'])) * 100
     except ZeroDivisionError:
-        sys.exit("wrong log file") # wrong log file may corrupt data
+        sys.exit("wrong energy log file")  # wrong log file may corrupt data
 
-    metric['total comsumption'] += metric['total transmit ticks'] + \
+    metric['total consumption'] += metric['total transmit ticks'] + \
         metric['total listen ticks']
-    with open(resultlog, 'w') as file:
-        file.write(f'{metric["radio on time"]}')
 
     return metric
 
